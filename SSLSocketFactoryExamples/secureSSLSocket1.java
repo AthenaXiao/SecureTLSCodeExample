5f7844be54ae2b916f98424a98314b2492023311
AthenaXiao <yaxiao2017@gmail.com>
2020-09-10 11:32:46 -0400

    wrap code snippets in main method

--- SSLSocketFactoryExamples/secureSSLSocket1.java
+++ SSLSocketFactoryExamples/secureSSLSocket1.java
@@ -1,13 +1,40 @@
-// create a SSLSocket
-SocketFactory sf = SSLSocketFactory.getDefault();
-SSLSocket socket = (SSLSocket) sf.createSocket("our.example.com", 443);
-//verify the hostname manually
-HostnameVerifier hv = HttpsURLConnection.getDefaultHostnameVerifier();
-if (!hv.verify(socket.getSession().getPeerHost(), socket.getSession())) {
-    throw new SSLHandshakeException("Hostname does not match!");
+import javax.net.SocketFactory;
+import javax.net.ssl.*;
+import java.io.*;
+
+public class secureSSLSocket1{
+
+    
+
+    public static void main(String [] args) throws Exception{
+
+
+
+        // create a SSLSocket
+        SocketFactory sf = SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) sf.createSocket("our.example.com", 443);
+        //verify the hostname manually
+        HostnameVerifier hv = HttpsURLConnection.getDefaultHostnameVerifier();
+        if (!hv.verify(socket.getSession().getPeerHost(), socket.getSession())) {
+            throw new SSLHandshakeException("Hostname does not match!");
+        }
+
+        // ... use socket ...
+        // Start handling application content
+        InputStream inputStream = socket.getInputStream();
+        OutputStream outputStream = socket.getOutputStream();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(outputStream));
+        // Write data
+        printWriter.print("Hello Server!");
+        printWriter.flush();
+
+
+        // communication ends
+        socket.close();
+
+
+
+    }
 }
-
-// ... use socket ...
-
-// communication ends
-socket.close();
--- SSLSocketFactoryExamples/secureSSLSocket2.java
+++ SSLSocketFactoryExamples/secureSSLSocket2.java
@@ -1,11 +1,39 @@
-// create a SSLSocket
-SocketFactory sf = SSLSocketFactory.getDefault();
-SSLSocket socket = (SSLSocket) sf.createSocket("our.example.com", 443);
+import javax.net.SocketFactory;
+import javax.net.ssl.*;
+import java.io.*;
 
-SSLParameters sslParams = new SSLParameters();
-sslParams.setEndpointIdentificationAlgorithm("HTTPS");
-socket.setSSLParameters(sslParams);
-// ... use socket ...
+public class secureSSLSocket2{
 
-// communication ends
-socket.close();
+
+
+    public static void main(String [] args) throws Exception{
+
+
+
+        // create a SSLSocket
+        SocketFactory sf = SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) sf.createSocket("our.example.com", 443);
+        //set algorithm as HTTPS to enable hostnameverifier
+        SSLParameters sslParams = new SSLParameters();
+        sslParams.setEndpointIdentificationAlgorithm("HTTPS");
+        socket.setSSLParameters(sslParams);
+
+        // ... use socket ...
+        // Start handling application content
+        InputStream inputStream = socket.getInputStream();
+        OutputStream outputStream = socket.getOutputStream();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(outputStream));
+        // Write data
+        printWriter.print("Hello Server!");
+        printWriter.flush();
+
+
+        // communication ends
+        socket.close();
+
+
+
+    }
+}
