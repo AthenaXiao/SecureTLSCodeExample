8b4e13256cc9221194de6403d2d4db2aad2aa491
AthenaXiao <yaxiao2017@gmail.com>
2020-09-10 11:37:11 -0400

    wrap hostnameVerifier example in main method

--- HostnameVerifierExamples/hostnameVerifierExample.java
+++ HostnameVerifierExamples/hostnameVerifierExample.java
@@ -1,17 +1,37 @@
-//custom a hostname verifier
-HostnameVerifier hostnameVerifier = new HostnameVerifier() {
-    @Override
-    public boolean verify(String hostname, SSLSession session) {
-        HostnameVerifier hv =
-            HttpsURLConnection.getDefaultHostnameVerifier();
-        return hv.verify("our.example.com", session);
-    }
-};
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import java.io.InputStream;
+import java.net.URL;
 
-// tell the URLConnection to use our HostnameVerifier
-URL url = new URL("https://our.example.org/");
-HttpsURLConnection conn =
-    (HttpsURLConnection)url.openConnection();
-conn.setHostnameVerifier(hostnameVerifier);
-InputStream in = conn.getInputStream();
-copyInputStreamToOutputStream(in, System.out);
+public class hostnameVerifierExample{
+
+
+
+    public static void main(String [] args) throws Exception{
+
+
+
+        //custom a hostname verifier
+        HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                HostnameVerifier hv =
+                        HttpsURLConnection.getDefaultHostnameVerifier();
+                return hv.verify("our.example.com", session);
+            }
+        };
+
+        // tell the URLConnection to use our HostnameVerifier
+        URL url = new URL("https://our.example.org/");
+        HttpsURLConnection conn =
+                (HttpsURLConnection)url.openConnection();
+        conn.setHostnameVerifier(hostnameVerifier);
+        InputStream in = conn.getInputStream();
+
+
+
+
+
+    }
+}
